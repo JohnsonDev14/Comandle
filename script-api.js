453e67f3731f068680be9ddca9a17d5ecf7357c7
G0dUssop <gus200809@gmail.com>
2025-01-23 16:27:37 -0300

    GG

--- script-api.js
+++ script-api.js
@@ -18,37 +18,41 @@ async function cartas (){
 
 cartas()
 
-async function aparecer() {
-    document.getElementById('form').addEventListener('submit',function(event) {
-        event.preventDefault();
-        
-        const nome = document.getElementById('nome').value;  
-        
-        const url = `https://api.scryfall.com/cards/named?fuzzy=${nome}`
-    
-        fetch(`${url}`)
-        .then(response => response.json())
-        .then(allCards =>{
-    
-            var singles = document.querySelector('.single-card')
-            singles.innerHTML=`
-            <img src="${allCards.image_uris.normal}" alt="">
-            <h3>${allCards.name}</h3>   
-             <h3>${allCards.type_line}</h3>   
-              <h3>${allCards.mana_cost}</h3>
-              <h3>Poder: ${allCards.power} Resistencia: ${allCards.toughness}</h3>    
-              
-            `
-        })
-    
-    });
-    
-    
+const singles = document.querySelector('.single-card')
+const nome = document.querySelector('#nome')
+
+const getPosts = async (nome) => {
+    const response = await fetch(
+        `https://api.scryfall.com/cards/search?as=grid&order=name&q=%28type%3Acreature+type%3Alegendary%29+%28game%3Apaper%29+legal%3Acommander+%28set%3Adsk+OR+set%3Ablb+OR+set%3Abig+OR+set%3Aotj+OR+set%3Amkm+OR+set%3Alci+OR+set%3Awoe+OR+set%3Amat+OR+set%3Amom+OR+set%3Aone+OR+set%3Abro+OR+set%3Admu+OR+set%3Asnc+OR+set%3Aneo+OR+set%3Avow+OR+set%3Amid+OR+set%3Astx+OR+set%3Akhm+OR+set%3Aznr+OR+set%3Aiko+OR+set%3Athb+OR+set%3Aeld+OR+set%3Awar+OR+set%3Arna+OR+set%3Agrn+OR+set%3Adom+OR+set%3Arix+OR+set%3Axln%29+name%3A${nome}`
+    );
+    return response.json();
 }
 
-aparecer()
+const cardsFromSearch = (carta) => {
+    return carta.map(item => `
+        <img src="${item.image_uris.normal}" alt="">
+        <h3>${item.name}</h3>   
+        <h3>${item.type_line}</h3>   
+        <h3>${item.mana_cost}</h3>
+        <h3>Poder: ${item.power} Resistencia: ${item.toughness}</h3> 
+    `).join('');
+}
 
+const searchPersonIntoDOM = async (nome) => {
+    const carta = await getPosts(nome);
+    const postsTemplate = cardsFromSearch(carta.data);
+    singles.insertAdjacentHTML('beforeend', postsTemplate);
+}
 
+const modifyInputValue = event => {
+    if (event.key === 'Enter') { // Verifica se a tecla pressionada é o Enter
+        const inputValue = event.target.value.trim().toLowerCase();
+        if (inputValue !== '') {
+            searchPersonIntoDOM(inputValue); // Realiza a busca
+        }
+    }
+};
 
+nome.addEventListener('keydown', modifyInputValue);
 
 
